ccfa11595e752863862e3278334afb0842e43a7f
Angel <angel.ivorra@gmail.com>
2013-02-01 00:22:15 +0100

    Initial commit

---
+++ Jqm.Tutorial01/src/net/aivorra/git/tutorial01/MainActivity.java
@@ -0,0 +1,15 @@
+package net.aivorra.git.tutorial01;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+}
